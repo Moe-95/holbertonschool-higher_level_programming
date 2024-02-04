d86cc74b9799c686ad70f331dd4ddf940ba142f7
Moe-95 <mhammad_fahs@hotmail.com>
2024-02-04 17:58:07 +0200

    3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 """
-This module contains a function that prints "My name is <first name> <last name>".
+    Insert here module comment
+
+    Write a function that prints My name is <first name> <last name>
+
+    Prototype: def say_my_name(first_name, last_name=""):
+    first_name and last_name must be strings otherwise,
+    raise a TypeError exception with the message
+    first_name must be a string or last_name must be a string
+    You are not allowed to import any module
+
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Prints "My name is <first name> <last name>".
-
-    Args:
-        first_name (str): The first name.
-        last_name (str): The last name.
-
-    Raises:
-        TypeError: If first_name or last_name is not a string.
-
-    """
-
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-
-    full_name = "My name is {} {}".format(first_name, last_name)
-    print(full_name.strip())
+    """ print my first and last name """
+    str_error = "first_name must be a string or last_name must be a string"
+    if type(first_name) is not str or type(last_name) is not str:
+        raise TypeError(str_error)
+    print("My name is", first_name, last_name)
