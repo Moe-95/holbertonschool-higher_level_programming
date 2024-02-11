5b5fe6b7c9e487f302b891ea5280f605727bc688
Moe-95 <mhammad_fahs@hotmail.com>
2024-02-11 20:32:33 +0200

    77

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry"""
+
 
 class BaseGeometry:
-    """A class representing base geometry."""
+    """this class represents a base geometry"""
 
     def area(self):
-        """Raise an exception with the message area() is not implemented."""
+        """method not implemented yet"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate the value as an integer."""
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        """validates a value as an integer
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-if __name__ == "__main__":
-    bg = BaseGeometry()
-
-    bg.integer_validator("my_int", 12)
-    bg.integer_validator("width", 89)
-
-    try:
-        bg.integer_validator("name", "John")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("age", 0)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("distance", -4)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+            raise ValueError("{} must be greater than 0".format(name))
