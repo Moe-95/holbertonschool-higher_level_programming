a7856fb828a216e2c22832018befd0212becb3ce
Moe-95 <mhammad_fahs@hotmail.com>
2024-02-11 14:29:06 +0200

    fix

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,21 +2,7 @@
 """Module for Square class."""
 
 
-class Rectangle:
-    """A class representing a rectangle."""
-
-    def __init__(self, width, height):
-        """Initialize the rectangle with width and height."""
-        self.width = width
-        self.height = height
-
-    def __str__(self):
-        """Return the rectangle description."""
-        return f"[Rectangle] {self.width}/{self.height}"
-
-    def area(self):
-        """Return the area of the rectangle."""
-        return self.width * self.height
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
@@ -30,25 +16,3 @@ class Square(Rectangle):
     def __str__(self):
         """Return the square description."""
         return f"[Square] {self.__size}/{self.__size}"
-
-    @property
-    def size(self):
-        """Return the size of the square."""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Set the size of the square."""
-        self.integer_validator("size", value)
-        self.__size = value
-
-    def integer_validator(self, name, value):
-        """Validate the value as an integer."""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-    def area(self):
-        """Return the area of the square."""
-        return self.__size ** 2
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,4 +3,6 @@
 
 
 def is_kind_of_class(obj, a_class):
+    """Check if an object is an instance of,
+      or inherited from, a specified class."""
     return isinstance(obj, a_class)
