d5dc266dbb1c1fe89c8f6be9ab0998c0cabf5b47
Moe-95 <mhammad_fahs@hotmail.com>
2024-02-11 14:53:50 +0200

    11

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 """
-Module for reading a text file.
+Write file module
 """
 
 
-def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints it to stdout.
-
-    Args:
-        filename (str): The name of the file to read.
-
-    Returns:
-        None
-    """
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, mode='w', encoding='utf-8') as file:
+        return file.write(text)
 
 
 if __name__ == "__main__":
-    read_file("my_file_0.txt")
+    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
